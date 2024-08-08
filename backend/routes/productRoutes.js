c6a8c5ffd4d9c8a708f24b9bdaf1374d71fe710c
Yair Yakobson <yairnet92@gmail.com>
2024-08-08 15:35:10 +0300

    change: small re-ordering

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,16 +1,22 @@
 import express from "express";
 const router = express.Router();
 
-import { getProduct, getProducts, getProductReviews } from "../controllers/products/read.js";
-import { newProduct, newProductReview } from "../controllers/products/create.js";
-import { updateProduct } from "../controllers/products/update.js";
-import { deleteProduct, deleteProductReview } from "../controllers/products/delete.js";
-import { getAdminProducts } from "../controllers/users/admin/adminGetProducts.js";
 import { isAuthenticatedUser, authorizeRoles } from "../middlewares/auth.js";
+
+import { newProduct } from "../controllers/products/create.js";
+import { getProduct, getProducts } from "../controllers/products/read.js";
+import { updateProduct } from "../controllers/products/update.js";
+import { deleteProduct } from "../controllers/products/delete.js";
+
+import { newProductReview } from "../controllers/reviews/create.js";
+import { getProductReviews } from "../controllers/reviews/read.js";
+import { deleteProductReview } from "../controllers/reviews/delete.js";
+
+import { getAdminProducts } from "../controllers/users/admin/adminGetProducts.js";
 
 router.route("/products").get(getProducts);
 router.route("/product/:id").get(getProduct);
-router.route("/product/new").post(newProduct);
+router.route("/product/new").post(isAuthenticatedUser, newProduct);
 
 router.route("/admin/products").get(isAuthenticatedUser, authorizeRoles("Admin"), getAdminProducts);
 router.route("/admin/product/:id")
