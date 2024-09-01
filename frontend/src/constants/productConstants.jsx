0556f34965b0b8b195227d4a893c2d337e4b9fe6
Yair Yakobson <yairnet92@gmail.com>
2024-09-01 12:49:37 +0300

    delete: deleted src folder
    drastic changes in UI code

--- frontend/src/constants/productConstants.jsx
+++
@@ -1,3 +0,0 @@
-export const PRODUCT_CATEGORIES = ["", "Manga", "Toys", "Miscellaneous"]
-export const PRODUCT_CONDITION = ["", "New", "Like New", "Refurbished"]
-export const ORDER_OPTIONS = ["Processing", "Shipped", "Delivered"]
